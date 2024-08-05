6c32dee0ddbd66db88afa476e4410ebad2562f8a
serj2626 <serj2626@mail.ru>
2024-08-05 14:32:39 +0300

    update page postdetail

--- frontend/src/components/Posts.vue
+++ frontend/src/components/Posts.vue
@@ -1,32 +1,37 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import axios from 'axios';
-import { ref, onMounted } from 'vue';
-import { useToast } from 'vue-toastification';
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import { useToast } from "vue-toastification";
+import { useRouter } from "vue-router";
 
-
+const router = useRouter();
 const toast = useToast();
 const posts = ref([]);
 
 const getPosts = async () => {
-    try {
-        const res = await axios.get('/api/workout/posts/');
-        console.log(res.data);
-        posts.value = res.data;
-    } catch {
-        toast.error('Произошла ошибка при получении постов');
-    }
-}
+  try {
+    const res = await axios.get("/api/workout/posts/");
+    console.log(res.data);
+    posts.value = res.data;
+  } catch {
+    toast.error("Произошла ошибка при получении постов");
+  }
+};
 
 onMounted(getPosts);
 </script>
 
 <template>
-      <div class="grid grid-cols-5 gap-5 mt-16">
-        <div 
-        v-for="post in posts" :key="post.id"
-        class="grid__box">{{ post.category }}</div>
-      </div>
+  <div class="grid grid-cols-5 gap-5 mt-16">
+    <div 
+    @click="router.push({name: 'post', params: {category: post.category}})"
+    v-for="post in posts" 
+    :key="post.id" 
+    class="grid__box">
+      {{ post.category }}
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- frontend/src/views/PostView.vue
+++ frontend/src/views/PostView.vue
@@ -3,25 +3,36 @@
 import axios from 'axios';
 import { ref, onMounted } from 'vue';
 import { useToast } from 'vue-toastification';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
 
 
+const { category } = route.params
 const toast = useToast();
 const post = ref([]);
 
-// const getPosts = async () => {
-//     try {
-//         const res = await axios.get('/api/workout/posts/');
-//         console.log(res.data);
-//         posts.value = res.data;
-//     } catch {
-//         toast.error('Произошла ошибка при получении постов');
-//     }
-// }
+const getPost = async () => {
+    try {
+        const res = await axios.get(`/api/workout/posts/${category}`);
+        console.log(res.data);
+        post.value = res.data;
+    } catch {
+        toast.error('Произошла ошибка при получении постов');
+    }
+}
 
-// onMounted(getPosts);
+onMounted(getPost);
 </script>
 
 <template>
+      <ArrowUpButton />
+    <div class="container row-2xl rounded-xl bg-white p-5 mb-12">
+        <h1 class="text-center mt-5 mb-20 uppercase font-bold">{{ post.title }}</h1>
+        <p v-html="post.content"></p>
+
+    </div>
 
 </template>
 
