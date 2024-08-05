518cab1f9fe2b47a2335e930f936af107eae3d3c
serj2626 <serj2626@mail.ru>
2024-08-05 14:17:07 +0300

    create component Post

---
+++ frontend/src/components/Posts.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+import { useToast } from 'vue-toastification';
+
+
+const toast = useToast();
+const posts = ref([]);
+
+const getPosts = async () => {
+    try {
+        const res = await axios.get('/api/workout/posts/');
+        console.log(res.data);
+        posts.value = res.data;
+    } catch {
+        toast.error('Произошла ошибка при получении постов');
+    }
+}
+
+onMounted(getPosts);
+</script>
+
+<template>
+      <div class="grid grid-cols-5 gap-5 mt-16">
+        <div 
+        v-for="post in posts" :key="post.id"
+        class="grid__box">{{ post.category }}</div>
+      </div>
+</template>
+
+<style scoped></style>
