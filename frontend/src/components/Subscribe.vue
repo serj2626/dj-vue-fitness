dbf40e699fb2c4d4cfeacf67d2b1623dd853f39f
serj2626 <serj2626@mail.ru>
2024-08-07 17:41:17 +0300

    update subscribe component

--- frontend/src/components/Subscribe.vue
+++ frontend/src/components/Subscribe.vue
@@ -65,32 +65,6 @@ const sendSubscription = async () => {
         </button>
       </form>
 
-      <svg
-        viewBox="0 0 1024 1024"
-        class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2"
-        aria-hidden="true"
-      >
-        <circle
-          cx="512"
-          cy="512"
-          r="512"
-          fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
-          fill-opacity="0.7"
-        ></circle>
-        <defs>
-          <radialGradient
-            id="759c1415-0410-454c-8f7c-9a820de03641"
-            cx="0"
-            cy="0"
-            r="1"
-            gradientUnits="userSpaceOnUse"
-            gradientTransform="translate(512 512) rotate(90) scale(512)"
-          >
-            <stop stop-color="#7775D6"></stop>
-            <stop offset="1" stop-color="#7ED321" stop-opacity="0"></stop>
-          </radialGradient>
-        </defs>
-      </svg>
       <Footer />
     </div>
   </div>
