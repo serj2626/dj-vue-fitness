e24bcac35a24afe58e49fd436380daaa8a28b903
serj2626 <serj2626@mail.ru>
2024-08-13 23:39:43 +0300

    create component CoachCard

---
+++ frontend/src/components/trainer/CoachCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+    coach: {
+        type: Object,
+        required: true
+    }
+})
+
+console.log("coach", coach);
+</script>
+
+<template>
+    <div class="card">
+        <div class="card__img">
+            <img :src="coach.avatar" alt="">
+        </div>  
+    </div>
+</template>
+
+
+<style scoped>
+
+.card{
+    max-height: 400px;
+    background-color: #FFF;
+    border-radius: 20px;
+    box-shadow: 0 0 15px rgb(219, 217, 217);
+}
+
+</style>
