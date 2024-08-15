c88b692c3b3a7006348deb9e885e68af45fe9f7a
serj2626 <serj2626@mail.ru>
2024-08-15 14:30:21 +0300

    update settings in main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,34 +1,49 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
-import axios from 'axios'
-import App from './App.vue'
-import router from './router'
+
+
+import axios from "axios";
+import App from "./App.vue";
+import router from "./router";
 import globalComponents from "./components/global";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
-import './assets/main.css'
+import "./assets/main.css";
 
 
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
-
-axios.defaults.baseURL = import.meta.env.VITE_API_URL
-
-
-const app = createApp(App)
-app.use(axios)
-    .use(PrimeVue, {
-        theme: {
-            preset: Aura
-        },
-        ripple: true
-        
-    })
-    .use(globalComponents)
-    .use(Toast)
-    .use(createPinia())
-    .use(router)
-    .mount('#app')
+const app = createApp(App);
+app
+  .use(axios)
+  .use(PrimeVue, {
+    theme: {
+      preset: Aura,
+    },
+    ripple: true,
+  })
+  .use(globalComponents)
+  .use(Toast, {
+    transition: "Vue-Toastification__fade",
+    maxToasts: 20,
+    newestOnTop: true,
+    position: "top-right",
+    timeout: 1500,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false,
+  })
+  .use(createPinia())
+  .use(router)
+  .mount("#app");
