d90facc5d18578be4afbe6cd1589adb1577cee7a
serj2626 <serj2626@mail.ru>
2024-08-27 14:16:16 +0300

    update component: reviewdetail

--- frontend/src/components/trainer/ReviewDetail.vue
+++ frontend/src/components/trainer/ReviewDetail.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { IReview } from '@/interfaces/workout';
-
+import { FwbRating } from 'flowbite-vue'
+import Rating from "primevue/rating";
 defineProps({
     review: {
         type: Object as () => IReview,
@@ -10,22 +11,27 @@ defineProps({
 </script>
 
 <template>
-        <li
-          class="reviews__desc flex flex-col gap-6 mx-2 p-2 rounded-md my-10"
+        <div
+          class="reviews__desc w-[85%] mx-auto flex flex-col gap-6 p-2 rounded-md my-10"
         >
           <div class="reviews-desc__header flex justify-between items-center">
-            <h3>{{ review.user }}</h3>
+            <div class="flex gap-3">
+              <h3>{{ review.user }}</h3>
+              <fwb-rating size="sm"  :rating="review.rating" />
+              <!-- <Rating disabled readonly :stars="5" :modelValue="review.rating" /> -->
+            </div>
+            
             <span>{{ review.created_at }}</span>
           </div>
           <div class="reviews-desc__body">
-            <p>{{ review.text }}.</p>
+            <p>{{ review.text }}</p>
           </div>
-        </li>
+        </div>
 </template>
 
 <style scoped>
 .reviews__desc {
-  background-color: rgb(2, 2, 2);
+  background-color: rgba(102, 93, 93, 0.223);
   color: white;
   box-shadow: 0 0 15px rgb(219, 217, 217);
 }
