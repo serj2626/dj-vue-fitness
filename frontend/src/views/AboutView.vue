fb34206f3fd010a1ee1d84577ed2c2bdfdfda78a
serj2626 <serj2626@mail.ru>
2024-08-27 14:18:29 +0300

    add flowbite comp

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,14 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useConfirm } from "primevue/useconfirm";
+import ConfirmDialog from 'primevue/confirmdialog';
+const confirm = useConfirm();
+const confirmPosition = (position: string) => {
+    confirm.require({
+        group: 'positioned',
+        message: 'Are you sure you want to continue?',
+        icon: 'pi pi-exclamation-triangle',
+        accept: () => {
+            alert(`Position: ${position}`);
+        },
+        reject: () => {
+            alert(`Position: ${position}`);
+        },
+    });
+}
+
+</script>
 
 <template>
-    <div class="container flex items-center about">
-        <div class="about__desc">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, debitis consectetur voluptates quidem quae fugit sequi id. Ea delectus error enim recusandae mollitia nisi nihil tempore provident sint aliquid eum beatae, quod officiis iure sequi aspernatur numquam quam aliquam magni perspiciatis excepturi atque blanditiis nemo? Corrupti iure explicabo ad nihil, veritatis tempore eveniet sapiente vitae blanditiis accusantium quam nobis fugiat dolorum, nisi delectus repudiandae, reprehenderit natus adipisci odio perspiciatis! Sit dolorum aut ipsa ut error quo aperiam. Amet mollitia placeat consequuntur, eveniet perferendis eum sit quas vel atque dolorem ratione explicabo, quia dignissimos sapiente nesciunt adipisci dolore fugit. Impedit, fugit.
+    <div class="container flex items-center about bg-slate-200">
+        <ConfirmDialog group="positioned"></ConfirmDialog>
+        <div class="flex flex-wrap justify-center gap-2 mb-4">
+            <Button @click="confirmPosition('topleft')" icon="pi pi-arrow-down-right" label="TopLeft"
+                severity="secondary" style="min-width: 10rem">3</Button>
+            <Button @click="confirmPosition('top')" icon="pi pi-arrow-down" label="Top" severity="secondary"
+                style="min-width: 10rem">2</Button>
+            <Button @click="confirmPosition('topright')" icon="pi pi-arrow-down-left" label="TopRight"
+                severity="secondary" style="min-width: 10rem">1</Button>
         </div>
-        <img src="@/assets/img/image.jpg" alt="" class="about__img">
     </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
