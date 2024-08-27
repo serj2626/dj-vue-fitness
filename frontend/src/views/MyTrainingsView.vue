5c2d2d8fb2faf7d06d1c15fcf9752ea7f3d01473
serj2626 <serj2626@mail.ru>
2024-08-27 14:21:20 +0300

    formatting code

--- frontend/src/views/MyTrainingsView.vue
+++ frontend/src/views/MyTrainingsView.vue
@@ -1,24 +1,35 @@
 <script setup lang="ts">
 import axios from "axios";
+
 import { useToast } from "vue-toastification";
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
+
 import type { IMyTrainig } from "@/interfaces/orders";
+
 import Paginator from "primevue/paginator";
-import Dropdown from 'primevue/dropdown';
-const selectedCity = ref();
-const cities = ref([
-  { name: "New York", code: "NY" },
-  { name: "Rome", code: "RM" },
-  { name: "London", code: "LDN" },
-  { name: "Istanbul", code: "IST" },
-  { name: "Paris", code: "PRS" },
-]);
+import Dropdown from "primevue/dropdown";
+
 const router = useRouter();
 const toast = useToast();
+
 const trainings = ref<IMyTrainig[]>([]);
 
-const show = ref(null);
+const selectTraining = ref(null);
+
+const choiceTraining = (trainId: number) => {
+  if (selectTraining.value === trainId) {
+    selectTraining.value = null;
+  } else {
+    selectTraining.value = trainId;
+  }
+};
+
+const actions = ref([
+  { name: "Удалить" },
+  { name: "Изменить" },
+  { name: "Оплатить" },
+]);
 
 const getMyTrainings = async () => {
   try {
@@ -34,7 +45,7 @@ onMounted(() => getMyTrainings());
 </script>
 
 <template>
-  <div class="m-16">
+  <div class="my-36 mx-16">
     <h1 class="text-3xl text-center text-uppercase text-yellow-500 py-4 mb-10">
       Мои тренировки
     </h1>
@@ -78,6 +89,8 @@ onMounted(() => getMyTrainings());
             <td class="w-4 p-4">
               <div class="flex items-center">
                 <input
+                  @click="choiceTraining(train.id)"
+                  :checked="selectTraining === train.id"
                   id="checkbox-table-search-1"
                   type="checkbox"
                   class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
@@ -113,8 +126,14 @@ onMounted(() => getMyTrainings());
             </td>
             <td class="px-6 py-4">
               <div class="card flex justify-content-center">
-      <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" class="w-full md:w-14rem" />
-  </div>
+                <Dropdown
+                  :disabled="selectTraining !== train.id"
+                  :options="actions"
+                  optionLabel="name"
+                  placeholder="Выбрать"
+                  class="w-full md:w-14rem"
+                />
+              </div>
             </td>
           </tr>
         </tbody>
@@ -126,8 +145,6 @@ onMounted(() => getMyTrainings());
       ></Paginator>
     </div>
   </div>
-
 </template>
 
 <style scoped></style>
-
