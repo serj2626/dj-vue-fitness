e110ddada2568e6f24cfe9b3091da3a57a85c15a
serj2626 <serj2626@mail.ru>
2024-08-30 13:59:54 +0300

    update my training page: add pagination

--- frontend/src/views/MyTrainingsView.vue
+++ frontend/src/views/MyTrainingsView.vue
@@ -4,37 +4,26 @@ import axios from "axios";
 import { useToast } from "vue-toastification";
 import { ref, onMounted } from "vue";
 import { useRouter } from "vue-router";
-
+import { FwbDropdown } from "flowbite-vue";
 import type { IMyTrainig } from "@/interfaces/orders";
 
 import Paginator from "primevue/paginator";
-import Dropdown from "primevue/dropdown";
+import DeleteModal from "@/components/DeleteModal.vue";
 
 const router = useRouter();
 const toast = useToast();
 
 const trainings = ref<IMyTrainig[]>([]);
 
-const selectTraining = ref(null);
+const currentPage = ref(1);
+const itemsPerPage = ref(5);
 
-const choiceTraining = (trainId: number) => {
-  if (selectTraining.value === trainId) {
-    selectTraining.value = null;
-  } else {
-    selectTraining.value = trainId;
-  }
-};
-
-const actions = ref([
-  { name: "Удалить" },
-  { name: "Изменить" },
-  { name: "Оплатить" },
-]);
+const selectTrain = ref(null);
 
 const getMyTrainings = async () => {
   try {
     const { data } = await axios.get("/api/auth/my-trainings/");
-    trainings.value = data;
+    trainings.value = data.results;
     console.log(trainings.value);
   } catch {
     toast.error("Произошла ошибка при получении тренировок");
@@ -58,18 +47,7 @@ onMounted(() => getMyTrainings());
           class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
         >
           <tr>
-            <th scope="col" class="p-4">
-              <div class="flex items-center">
-                <input
-                  id="checkbox-all-search"
-                  type="checkbox"
-                  class="hidden w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label for="checkbox-all-search" class="sr-only"
-                  >checkbox</label
-                >
-              </div>
-            </th>
+            <th scope="col" class="px-6 py-3">№</th>
             <th scope="col" class="px-6 py-3">Тренер</th>
             <th scope="col" class="px-6 py-3">Позиция</th>
             <th scope="col" class="px-6 py-3">Начало</th>
@@ -82,24 +60,11 @@ onMounted(() => getMyTrainings());
         </thead>
         <tbody>
           <tr
-            v-for="train in trainings"
+            v-for="(train, index) in trainings"
             :key="train.id"
             class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
           >
-            <td class="w-4 p-4">
-              <div class="flex items-center">
-                <input
-                  @click="choiceTraining(train.id)"
-                  :checked="selectTraining === train.id"
-                  id="checkbox-table-search-1"
-                  type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label for="checkbox-table-search-1" class="sr-only"
-                  >checkbox</label
-                >
-              </div>
-            </td>
+            <td class="px-6 py-4">{{ index + 1 }}</td>
             <td class="px-6 py-4">
               <a
                 class="text-blue-600 hover:underline cursor-pointer"
@@ -125,25 +90,43 @@ onMounted(() => getMyTrainings());
               <i class="fa-solid fa-xmark fa-2xl" style="color: #ff0000"></i>
             </td>
             <td class="px-6 py-4">
-              <div class="card flex justify-content-center">
-                <Dropdown
-                  :disabled="selectTraining !== train.id"
-                  :options="actions"
-                  optionLabel="name"
-                  placeholder="Выбрать"
-                  class="w-full md:w-14rem"
-                />
-              </div>
+              <fwb-dropdown placement="left" text="Выбрать">
+                <div
+                  class="w-52 border-2 border-slate-300 rounded-lg shadow-xl shadow-zinc-400"
+                >
+                  <p 
+                  @click="selectTrain = train"
+                  class="p-2 hover:bg-slate-200 cursor-pointer">Удалить</p>
+                  <p
+                    @click="
+                      router.push({
+                        name: 'payTraining',
+                        params: { id: train.id },
+                      })
+                    "
+                    class="p-2 hover:bg-slate-200 cursor-pointer"
+                  >
+                    Оплатить
+                  </p>
+                </div>
+              </fwb-dropdown>
             </td>
           </tr>
         </tbody>
       </table>
-      <Paginator
-        :rows="10"
-        :totalRecords="120"
-        :rowsPerPageOptions="[10, 20, 30]"
-      ></Paginator>
+
+      <Paginator 
+      v-model="currentPage" 
+      :rows="itemsPerPage" 
+      :totalRecords="trainings.length">
+      </Paginator>
     </div>
+
+    <DeleteModal
+    :text="`Вы действительно хотите удалить тренировку?`"
+    v-if="selectTrain" 
+    @closeModal="selectTrain = null" 
+    @delete="getMyTrainings" />
   </div>
 </template>
 
