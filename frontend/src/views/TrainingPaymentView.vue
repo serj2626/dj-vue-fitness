5d64c1952983c0121b10cafe6d5a7f633b443984
serj2626 <serj2626@mail.ru>
2024-08-30 13:58:59 +0300

    new page: train payment

---
+++ frontend/src/views/TrainingPaymentView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+
+
+const route = useRoute();
+</script>
+<template>
+<div class="container border-2 border-white rounded-lg text-orange-400">
+ {{ route.params.id }}
+</div>
+</template>
+<style scoped>
+ 
+</style>
