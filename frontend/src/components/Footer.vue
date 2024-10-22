e47db8932ee0cd9721e94ff67dab162fca662cfe
serj2626 <serj2626@mail.ru>
2024-10-22 12:09:17 +0300

    style: removed spaces

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,9 +1,10 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+import { footerLinks } from "@/types/footer.data";
+</script>
 <template>
   <div
-    class="w-[50%] mx-auto flex justify-between items-center h-16 absolute bottom-0 left-[50%] translate-x-[-50%]"
+    class="w-[50%] mx-auto flex justify-between items-center h-16 absolute 
+    bottom-0 left-[50%] translate-x-[-50%]"
   >
     <div class="text-white">
       <a class="text-yellow-500" href="https://github.com/serj2626"
@@ -12,33 +13,15 @@
       <p>© 2024 Санкт-Петербург</p>
     </div>
     <ul class="flex items-center gap-2">
-      <li class="ms-3 footer-link">
-        <a class="text-white" href="#"
-          ><i class="fa-brands fa-telegram fa-xl" style="color: #74c0fc"></i
-        ></a>
-      </li>
-      <li class="ms-3 footer-link">
-        <a class="text-white" href="#"
-          ><i class="fa-brands fa-instagram fa-xl" style="color: #ffd43b"></i
-        ></a>
-      </li>
-      <li class="ms-3 footer-link">
-        <a class="text-white" href="#"
-          ><i class="fa-brands fa-linkedin fa-xl" style="color: #74c0fc"></i
-        ></a>
-      </li>
-      <li class="ms-3 footer-link">
-        <a class="text-white" href="#"
-          ><i class="fa-brands fa-vk fa-xl" style="color: #74c0fc"></i
-        ></a>
-      </li>
-      <li class="ms-3 footer-link">
-        <a class="text-white" href="#"
-          ><i class="fa-brands fa-github fa-xl" style="color: white"></i
+      <li
+        v-for="(link, index) in footerLinks"
+        :key="index"
+        class="ms-3 footer-link"
+      >
+        <a class="text-white" :href="link.link" :title="link.title">
+          <i :class="link.icon" :style="link.style"></i
         ></a>
       </li>
     </ul>
   </div>
 </template>
-
-<style scoped></style>
