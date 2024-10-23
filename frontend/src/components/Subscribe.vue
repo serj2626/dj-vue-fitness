cd712c50131bafb025bf8076bcdbc6e2c4881063
serj2626 <serj2626@mail.ru>
2024-10-24 00:25:37 +0300

    feat: Changed the button and input to components flowbite

--- frontend/src/components/Subscribe.vue
+++ frontend/src/components/Subscribe.vue
@@ -1,9 +1,10 @@
-<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import { ref } from "vue";
 import Footer from "../components/Footer.vue";
 import axios from "axios";
 import { useToast } from "vue-toastification";
+import { FwbButton } from 'flowbite-vue'
+import { FwbInput } from 'flowbite-vue'
 
 const toast = useToast();
 const subscription = ref<string>("");
@@ -16,7 +17,7 @@ const sendSubscription = async () => {
     });
     toast.success("Вы подписались на рассылку");
     subscription.value = "";
-  } catch (err ) {
+  } catch (err: any ) {
     let [error]: string = err.response.data.email;
     if (error) toast.error("Вы уже подписаны на рассылку");
     else toast.error("Что-то пошло не так");
@@ -45,24 +46,12 @@ const sendSubscription = async () => {
         @submit.prevent="sendSubscription"
         class="mx-auto mt-5 flex max-w-md gap-x-4"
       >
-        <label for="email-address" class="sr-only">Почта</label>
-        <input
-          v-model="subscription"
-          id="email-address"
-          name="email"
-          type="email"
-          autocomplete="email"
-          required
-          class="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-          placeholder="Введите вашу почту"
-        />
+            <fwb-input
+              v-model="subscription"
+              placeholder="Введите почту"
 
-        <button
-          type="submit"
-          class="flex-none rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-        >
-          Подписаться
-        </button>
+        />
+        <fwb-button size="sm" color="light">Подписаться</fwb-button>
       </form>
 
       <Footer />
