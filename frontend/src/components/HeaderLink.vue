f91292427129abb6c2a02a8cdd71179015d52341
serj2626 <serj2626@mail.ru>
2024-10-24 00:45:29 +0300

    feat: create component for header link

---
+++ frontend/src/components/HeaderLink.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
+let routeName = computed(() => route.name);
+import type { IHeaderLink } from "@/types/header.data";
+
+defineProps({
+  link: {
+    type: Object as () => IHeaderLink,
+    required: true,
+  },
+});
+</script>
+<template>
+  <a v-if="routeName === 'home'" :href="link.link" :class="link.class">
+    {{ link.title }}
+  </a>
+</template>
+<style scoped></style>
