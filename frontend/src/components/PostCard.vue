70666383800fd4752fec84300e16838c4b55e933
serj2626 <serj2626@mail.ru>
2024-10-24 01:10:00 +0300

    feat: Create component for detail post

---
+++ frontend/src/components/PostCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { IPost } from "@/types/types";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+defineProps({
+  post: {
+    type: Object as () => IPost,
+    required: true,
+  },
+});
+</script>
+<template>
+  <li
+    @click="router.push({ name: 'post', params: { category: post.category } })"
+    class="grid__box post"
+  >
+    {{ post.category }}
+  </li>
+</template>
