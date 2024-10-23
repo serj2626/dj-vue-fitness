a1c831c83d6d2ed03fac4dc46e67ef638c3ff19c
serj2626 <serj2626@mail.ru>
2024-10-24 01:03:26 +0300

    feat: Create new component for abonement detail

---
+++ frontend/src/components/abonements/AbonementCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { IAbonement } from "@/types/orders";
+
+defineProps({
+    abonement: {
+        type: Object as () => IAbonement,
+        required: true,
+    },
+})
+</script>
+<template>
+    <div
+      class="box flex flex-col gap-2 items-center justify-around">
+      <div class="box__title">
+        <h3 class="title text-xl uppercase font-bold">{{ abonement.title }}</h3>
+      </div>
+      <div class="box__body text-center">
+        <p class="text-white mb-4">{{ abonement.description }}</p>
+        <span class="text-gray-400">{{ abonement.price }} рублей</span>
+      </div>
+    </div>
+</template>
