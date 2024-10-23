fc74f027509fab789a98d4fa95bd08e0f7a08eea
serj2626 <serj2626@mail.ru>
2024-10-24 01:02:54 +0300

    style: Removed spaces

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -8,7 +8,6 @@ import { headerLinks } from "@/types/header.data";
 import HeaderLink from "./HeaderLink.vue";
 import { ref } from "vue";
 
-
 const showProfile = ref(false);
 const closeShowProfile = () => {
   setTimeout(() => {
@@ -17,7 +16,6 @@ const closeShowProfile = () => {
 };
 
 const router = useRouter();
-
 
 const toast = useToast();
 const store = useUserStore();
@@ -31,20 +29,40 @@ const logout = () => {
 </script>
 
 <template>
-  <div id="navigation" class="header bg-black bg-opacity-50 py-2 fixed z-10 top-0 w-full">
-    <div class="header__container container h-20 flex justify-center flex-wrap items-center gap-6">
+  <div
+    id="navigation"
+    class="header bg-black bg-opacity-50 py-2 fixed z-10 top-0 w-full"
+  >
+    <div
+      class="header__container container h-20 flex justify-center flex-wrap items-center gap-6"
+    >
       <h3 class="header__logo text-yellow-300 font-bold text-lg mr-14">
         <RouterLink :to="{ name: 'home' }">DV FITNESS </RouterLink>
       </h3>
       <div class="header__menu flex items-center gap-4">
-        <HeaderLink v-for="(link, index) in headerLinks" :key="index" :link="link" />
-        <RouterLink v-if="!auth.isAuthenticated" :to="{ name: 'login' }" class="header__link">Войти
+        <HeaderLink
+          v-for="(link, index) in headerLinks"
+          :key="index"
+          :link="link"
+        />
+        <RouterLink
+          v-if="!auth.isAuthenticated"
+          :to="{ name: 'login' }"
+          class="header__link"
+          >Войти
         </RouterLink>
 
-        <div v-if="auth.isAuthenticated" @click="showProfile = true" @mouseleave="closeShowProfile"
-          class="flex flex-col items-center relative z-50">
+        <div
+          v-if="auth.isAuthenticated"
+          @click="showProfile = true"
+          @mouseleave="closeShowProfile"
+          class="flex flex-col items-center relative z-50"
+        >
           <a class="header__link cursor-pointer">Профиль</a>
-          <ul :class="showProfile ? 'showDropdownMenu' : 'hidden'" class="text-white">
+          <ul
+            :class="showProfile ? 'showDropdownMenu' : 'hidden'"
+            class="text-white"
+          >
             <RouterLink :to="{ name: 'myAbonements' }">
               <li>Абонементы</li>
             </RouterLink>
@@ -56,8 +74,14 @@ const logout = () => {
             </RouterLink>
           </ul>
         </div>
-        <a v-if="auth.isAuthenticated" @click="logout()" class="header__link">Выйти</a>
-        <RouterLink :to="{ name: 'abonements' }" class="header__link header__link__buy">Купить абонемент</RouterLink>
+        <a v-if="auth.isAuthenticated" @click="logout()" class="header__link"
+          >Выйти</a
+        >
+        <RouterLink
+          :to="{ name: 'abonements' }"
+          class="header__link header__link__buy"
+          >Купить абонемент</RouterLink
+        >
       </div>
     </div>
   </div>
