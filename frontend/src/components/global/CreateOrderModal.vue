0c8ebaae43b6fee58198e6e0932d5d804f0c0480
serj2626 <serj2626@mail.ru>
2024-10-24 22:08:32 +0300

    feat: Add feature for create order training

--- frontend/src/components/global/CreateOrderModal.vue
+++ frontend/src/components/global/CreateOrderModal.vue
@@ -1,52 +1,45 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 import { FwbButton, FwbModal } from "flowbite-vue";
-import type { ITrainerInfo, iRate } from "@/types/workout";
+import type { ITrainer, iRate } from "@/types/workout";
 import type { IAbonement } from "@/types/orders";
 
 
 const date = defineModel("date");
+const rate = defineModel("rate");
 
 type OrderType = "training" | "abonement";
 
 interface IProps {
     abonement?: IAbonement;
-    coach?: ITrainerInfo;
+    coach?: ITrainer;
     orderType: OrderType;
     rates?: iRate[]
 }
 
 defineProps<IProps>();
 
-const isShowModal = ref(false);
-
-function closeModal() {
-  isShowModal.value = false;
-}
-function showModal() {
-  isShowModal.value = true;
-}
 
 defineEmits(["closeModal", "createOrderTraining", "crateOrderAbonement"]);
 </script>
 <template>
-  <fwb-modal v-if="orderType === 'training'" @close="closeModal">
+  <fwb-modal v-if="orderType === 'training'" @close="$emit('closeModal')">
     <template #header>
       <div class="flex items-center text-lg">Бронирование тренировки</div>
     </template>
     <template #body>
         <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-            {{ coach?.firstName + " " + coach?.lastName }} - {{ coach?.position }}
+            {{ coach?.first_name + " " + coach?.last_name }} - {{ coach?.position }}
           </h3>
-        <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <label for="date" class="block my-2 text-sm font-medium text-gray-900 dark:text-white">
             Выберите дату занятия</label>
-          <input id=date  class="w-full p-2 border border-gray-300 rounded" type="datetime-local" />
+          <input id=date  v-model="date" class="w-full p-2 border border-gray-300 rounded" type="datetime-local" />
           
         <label for="small" class="block my-2 text-sm font-medium text-gray-900 dark:text-white">
             Выберите тариф
         </label>
 
-          <select id="small"
+          <select id="small" v-model="rate"
             class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             <option v-for="rate in rates" :key="rate.id" :value="rate.id">
               {{ rate.title }} - {{ rate.count_minutes }} мин по цене {{ rate.price }} рублей
@@ -60,15 +53,24 @@ defineEmits(["closeModal", "createOrderTraining", "crateOrderAbonement"]);
     </template>
     <template #footer>
       <div class="flex justify-between">
-        <fwb-button @click="closeModal" color="red">
+        <fwb-button 
+          size="lg" 
+          @click="$emit('closeModal')" 
+          color="red"
+        >
           Отмена
         </fwb-button>
-        <fwb-button @click="closeModal" color="green">
-            Записаться
+        <fwb-button 
+          size="lg" 
+          @click="$emit('createOrderTraining')" 
+          color="green"
+        >
+          Записаться
         </fwb-button>
       </div>
     </template>
   </fwb-modal>
+
 
   <fwb-modal v-if="orderType === 'abonement'" @close="$emit('closeModal')">
     <template #header>
@@ -94,11 +96,19 @@ defineEmits(["closeModal", "createOrderTraining", "crateOrderAbonement"]);
     </template>
     <template #footer>
       <div class="flex justify-between">
-        <fwb-button @click="$emit('closeModal')" color="red">
+        <fwb-button
+          size="lg" 
+          @click="$emit('closeModal')" 
+          color="red"
+        >
           Отмена
         </fwb-button>
-        <fwb-button @click="$emit('crateOrderAbonement')" color="green">
-            Забронировать
+        <fwb-button 
+          size="lg" 
+          @click="$emit('crateOrderAbonement')" 
+          color="green"
+        >
+          Забронировать
         </fwb-button>
       </div>
     </template>
