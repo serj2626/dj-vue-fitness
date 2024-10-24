6cb5238a7277fc2c028f3ed27e23a14fa3d9dd59
serj2626 <serj2626@mail.ru>
2024-10-25 01:59:13 +0300

    style: Deleted unnecessary global components

--- frontend/src/components/global/index.ts
+++ frontend/src/components/global/index.ts
@@ -1,19 +1,13 @@
 import type { App } from "vue";
 import ArrowUpButton from "./ArrowUpButton.vue";
-import UModal from "./UModal.vue";
 import URating from "./URating.vue";
-import USelect from "./USelect.vue";
-import UInput from "./UInput.vue";
 import UButton from "./UButton.vue";
 import AuthCard from "./AuthCard.vue";
 import CreateOrderModal from "./CreateOrderModal.vue";
 
 const components = [
   { name: "ArrowUpButton", component: ArrowUpButton },
-  { name: "UModal", component: UModal },
   { name: "URating", component: URating },
-  { name: "USelect", component: USelect },
-  { name: "UInput", component: UInput },
   { name: "UButton", component: UButton },
   { name: "AuthCard", component: AuthCard },
   { name: "CreateOrderModal", component: CreateOrderModal },
