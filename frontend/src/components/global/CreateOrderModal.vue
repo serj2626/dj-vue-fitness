66905a797cfcb1f7cd7d9294f4a601e1544f4c8a
serj2626 <serj2626@mail.ru>
2024-10-24 22:41:20 +0300

    refactor: Update title in modal for create order abonement

--- frontend/src/components/global/CreateOrderModal.vue
+++ frontend/src/components/global/CreateOrderModal.vue
@@ -8,7 +8,7 @@ import type { IAbonement } from "@/types/orders";
 const date = defineModel("date");
 const rate = defineModel("rate");
 
-type OrderType = "training" | "abonement";
+type OrderType = "training" | "abonement" | "review"
 
 interface IProps {
     abonement?: IAbonement;
@@ -20,9 +20,11 @@ interface IProps {
 defineProps<IProps>();
 
 
-defineEmits(["closeModal", "createOrderTraining", "crateOrderAbonement"]);
+defineEmits(["closeModal", "createOrderTraining", "crateOrderAbonement", "createReview"]);
 </script>
 <template>
+                <!-- Бронирование тренировки -->
+
   <fwb-modal v-if="orderType === 'training'" @close="$emit('closeModal')">
     <template #header>
       <div class="flex items-center text-lg">Бронирование тренировки</div>
@@ -71,10 +73,11 @@ defineEmits(["closeModal", "createOrderTraining", "crateOrderAbonement"]);
     </template>
   </fwb-modal>
 
+                      <!-- Бронирование абонемента   -->
 
   <fwb-modal v-if="orderType === 'abonement'" @close="$emit('closeModal')">
     <template #header>
-      <div class="flex items-center text-lg">Бронрование абонемента</div>
+      <div class="flex items-center text-lg">Бронирование абонемента</div>
     </template>
     <template #body>
         <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
@@ -113,5 +116,55 @@ defineEmits(["closeModal", "createOrderTraining", "crateOrderAbonement"]);
       </div>
     </template>
   </fwb-modal>
+
+                             <!-- Оставьте свой отзыв -->
+
+<fwb-modal v-if="orderType === 'review'" @close="$emit('closeModal')">
+  <template #header>
+    <div class="flex items-center text-lg">Оставьте свой отзыв</div>
+  </template>
+  <template #body>
+      <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+          {{ coach?.first_name + " " + coach?.last_name }} - {{ coach?.position }}
+        </h3>
+      <label for="date" class="block my-2 text-sm font-medium text-gray-900 dark:text-white">
+          Выберите дату занятия</label>
+        <input id=date  v-model="date" class="w-full p-2 border border-gray-300 rounded" type="datetime-local" />
+        
+      <label for="small" class="block my-2 text-sm font-medium text-gray-900 dark:text-white">
+          Выберите тариф
+      </label>
+
+        <select id="small" v-model="rate"
+          class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <option v-for="rate in rates" :key="rate.id" :value="rate.id">
+            {{ rate.title }} - {{ rate.count_minutes }} мин по цене {{ rate.price }} рублей
+          </option>
+        </select>
+        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+          <span class="font-bold text-xl block underline"> </span>
+          Вы можете записаться на персональную тренировку. Для записи выберите
+          начальную дату занятия,тариф и нажмите кнопку "Записаться".
+        </p>
+  </template>
+  <template #footer>
+    <div class="flex justify-between">
+      <fwb-button 
+        size="lg" 
+        @click="$emit('closeModal')" 
+        color="red"
+      >
+        Отмена
+      </fwb-button>
+      <fwb-button 
+        size="lg" 
+        @click="$emit('createReview')" 
+        color="green"
+      >
+        Отправить
+      </fwb-button>
+    </div>
+  </template>
+</fwb-modal>
+
 </template>
-<style scoped></style>
