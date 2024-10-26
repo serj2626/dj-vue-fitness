a9498be905190e93b39bac9a068ab476d93e55ae
serj2626 <serj2626@mail.ru>
2024-10-26 13:56:30 +0300

    feat: Add import for global component

--- frontend/src/components/global/index.ts
+++ frontend/src/components/global/index.ts
@@ -4,6 +4,8 @@ import URating from "./URating.vue";
 import UButton from "./UButton.vue";
 import AuthCard from "./AuthCard.vue";
 import CreateOrderModal from "./CreateOrderModal.vue";
+import UModal from "./UModal.vue";
+import DeleteModal from "./DeleteModal.vue";
 
 const components = [
   { name: "ArrowUpButton", component: ArrowUpButton },
@@ -11,6 +13,8 @@ const components = [
   { name: "UButton", component: UButton },
   { name: "AuthCard", component: AuthCard },
   { name: "CreateOrderModal", component: CreateOrderModal },
+  { name: "UModal", component: UModal },
+  { name: "DeleteModal", component: DeleteModal },
 ];
 
 export default {
