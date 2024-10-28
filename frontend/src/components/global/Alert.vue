c5dbbd990d75fa0a4e0240bad6fda680a609b3aa
serj2626 <serj2626@mail.ru>
2024-10-28 14:08:20 +0300

    feat: created global alert component

--- frontend/src/components/global/Alert.vue
+++ frontend/src/components/global/Alert.vue
@@ -1,11 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import { FwbAlert } from 'flowbite-vue'
+type AlertType = "success" | "danger" | "warning" | "info"
 
+defineProps<{
+    view: AlertType
+    message: string 
+}>()
 </script>
 <template>
-<div>
- 
-</div>
+    <fwb-alert icon :type="view">
+        {{ message }}
+    </fwb-alert>
 </template>
-<style scoped>
- 
-</style>
