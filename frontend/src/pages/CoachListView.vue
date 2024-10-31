7b751698bd41f822ca1e97bb1c521e3e4fab4506
serj2626 <serj2626@mail.ru>
2024-10-31 22:19:06 +0300

    feat: added alert if coaches list not exist

--- frontend/src/pages/CoachListView.vue
+++ frontend/src/pages/CoachListView.vue
@@ -12,8 +12,7 @@ const trainerInfo = reactive<ITrainerInfo>({
   firstName: "",
   lastName: "",
   position: "",
-})
-
+});
 
 const showDetails = (trainer: ITrainer) => {
   trainerInfo.id = trainer.id;
@@ -29,7 +28,6 @@ const resetDetails = () => {
   trainerInfo.id = "";
 };
 
-
 const toast = useToast();
 
 const searchName = ref("");
@@ -37,7 +35,10 @@ const selected = ref("");
 console.log(selected.value);
 const categories = [
   { value: "Инструктор бассейна", name: "Инструктор бассейна" },
-  { value: "Инструктор тренажерного зала", name: "Инструктор тренажорного зала" },
+  {
+    value: "Инструктор тренажерного зала",
+    name: "Инструктор тренажорного зала",
+  },
   { value: "Инструктор йоги", name: "Тренер по йоге" },
 ];
 
@@ -54,17 +55,28 @@ const getCoaches = async () => {
 
 const totalCoaches = computed<ITrainer[]>(() => {
   if (selected.value && searchName.value) {
-    return coaches.value.filter((coach) => coach.position === selected.value)
-      .filter((coach) => coach.first_name.toLowerCase().includes(searchName.value.toLowerCase()) 
-      || coach.last_name.toLowerCase().includes(searchName.value.toLowerCase()))
+    return coaches.value
+      .filter((coach) => coach.position === selected.value)
+      .filter(
+        (coach) =>
+          coach.first_name
+            .toLowerCase()
+            .includes(searchName.value.toLowerCase()) ||
+          coach.last_name.toLowerCase().includes(searchName.value.toLowerCase())
+      );
   } else if (selected.value) {
-    return coaches.value.filter((coach) => coach.position === selected.value)
+    return coaches.value.filter((coach) => coach.position === selected.value);
   } else {
     if (searchName.value) {
-    return coaches.value.filter((coach) => coach.first_name.toLowerCase().includes(searchName.value.toLowerCase()) 
-    || coach.last_name.toLowerCase().includes(searchName.value.toLowerCase()))
-  }
-    return coaches.value
+      return coaches.value.filter(
+        (coach) =>
+          coach.first_name
+            .toLowerCase()
+            .includes(searchName.value.toLowerCase()) ||
+          coach.last_name.toLowerCase().includes(searchName.value.toLowerCase())
+      );
+    }
+    return coaches.value;
   }
 });
 
@@ -73,50 +85,57 @@ onMounted(getCoaches);
 
 <template>
   <div class="container row my-36">
-    <h1 class="text-xl sm:text-2xl md:text-3xl text-center text-uppercase text-yellow-300 mb-12">
-      Тренеры
-    </h1>
-    <div class="flex justify-between items-center">
-      <fwb-select
-        v-model="selected"
-        :options="categories"
-        placeholder="Выберите категорию"
-        size="sm"
-      />
-
-      <fwb-input
-        v-model="searchName"
-        placeholder="Введите имя тренера"
+    <div v-if="coaches.length">
+      <h1
+        class="text-xl sm:text-2xl md:text-3xl text-center text-uppercase text-yellow-300 mb-12"
       >
-        <template #prefix>
-          <svg
-            aria-hidden="true"
-            class="w-5 h-5 text-gray-500 dark:text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-            />
-          </svg>
-        </template>
-      </fwb-input>
+        Тренеры
+      </h1>
+      <div class="flex justify-between items-center">
+        <fwb-select
+          v-model="selected"
+          :options="categories"
+          placeholder="Выберите категорию"
+          size="sm"
+        />
+
+        <fwb-input v-model="searchName" placeholder="Введите имя тренера">
+          <template #prefix>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+              />
+            </svg>
+          </template>
+        </fwb-input>
+      </div>
+
+      <div
+        class="grid max-[450px]:grid-cols-1 grid-cols-2 md:grid-cols-3 gap-10 mt-12 px-3"
+      >
+        <CoachCard
+          v-for="coach in totalCoaches"
+          :key="coach.id"
+          :coach="coach"
+          :currentId="trainerInfo.id"
+          @showDetail="showDetails(coach)"
+          @resetData="resetDetails"
+        />
+      </div>
     </div>
 
-    <div class="grid max-[450px]:grid-cols-1 grid-cols-2 md:grid-cols-3 gap-10 mt-12 px-3">
-      <CoachCard 
-        v-for="coach in totalCoaches" 
-        :key="coach.id" 
-        :coach="coach"
-        :currentId="trainerInfo.id"
-        @showDetail = "showDetails(coach)"
-        @resetData = "resetDetails"  
-      />
+    <div v-else class="h-[800px] flex justify-center items-center">
+      <Alert view="danger" message="На данный момент тренеров нет" />
     </div>
   </div>
 </template>
