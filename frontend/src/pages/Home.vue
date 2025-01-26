8b58ed136151a664ac6f96c2da180353ac4e007d
serj2626 <serj2626@mail.ru>
2025-01-26 23:10:39 +0300

    feat: updated imports

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import AboutSection from "@/components/sections/AboutSection.vue";
 import CoachList from "../components/sections/CoachListSection.vue";
-import AbonementsList from "@/components/sections/AbonementsList.vue";
+import AbonementsList from "@/components/sections/AbonementsListSection.vue";
 import HeaderSection from "@/components/sections/HeaderSection.vue";
 import SubscribeSection from "@/components/sections/SubscribeSection.vue";
 import PoolSection from "@/components/sections/PoolSection.vue";
