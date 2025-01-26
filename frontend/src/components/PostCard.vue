20d3e37f38d659d758a66325cd025f4c4372082a
serj2626 <serj2626@mail.ru>
2025-01-26 23:11:47 +0300

    feat: add style for post

--- frontend/src/components/PostCard.vue
+++ frontend/src/components/PostCard.vue
@@ -3,18 +3,31 @@ import type { IPost } from "@/stores/posts";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
-defineProps({
-  post: {
-    type: Object as () => IPost,
-    required: true,
-  },
-});
+defineProps<{
+  post: IPost;
+}>();
 </script>
 <template>
   <li
     @click="router.push({ name: 'post', params: { category: post.category } })"
-    class="grid__box post"
+    class="post"
   >
     {{ post.category }}
   </li>
 </template>
+<style scoped lang="scss">
+.post {
+  display: flex;
+  align-items: center;
+  margin-top: 100px;
+  flex-direction: column;
+  cursor: pointer;
+  border-radius: 10px;
+  padding: 40px;
+  outline: 2px solid;
+  box-shadow: 0 0 10px #00000038;
+
+  text-transform: uppercase;
+  color: orange;
+}
+</style>
