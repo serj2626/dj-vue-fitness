e00bd83b2341c42a7d4e4ff4d9d7a0d014a45659
serj2626 <serj2626@mail.ru>
2025-01-26 14:43:13 +0300

    feat: new file  by faq

---
+++ frontend/src/components/sections/FAQ.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { faqs } from "@/types/faq";
+import {
+  FwbAccordion,
+  FwbAccordionContent,
+  FwbAccordionHeader,
+  FwbAccordionPanel,
+} from "flowbite-vue";
+</script>
+<template>
+  <div class="faq py-6 mb-3">
+    <div class="container faq__container">
+      <h3 class="faq__title text-3xl text-center text-yellow-300 mb-16">Ответы на часто задаваемые вопросы</h3>
+      <fwb-accordion :open-first-item="false">
+        <fwb-accordion-panel v-for="faq in faqs" :key="faq.id">
+          <fwb-accordion-header>{{ faq.question }}</fwb-accordion-header>
+          <fwb-accordion-content>
+            <div>
+              <p class="mb-2 text-gray-500 dark:text-gray-400">
+                {{ faq.answer }}
+              </p>
+            </div>
+          </fwb-accordion-content>
+        </fwb-accordion-panel>
+      </fwb-accordion>
+    </div>
+  </div>
+</template>
