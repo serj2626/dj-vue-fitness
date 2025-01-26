b2d7de16a28cd6d78a6799011bb3a7cdb4155ffa
serj2626 <serj2626@mail.ru>
2025-01-26 14:43:37 +0300

    feat: add section faq

--- frontend/src/pages/HomeView.vue
+++ frontend/src/pages/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import AboutSection from "@/components/sections/AboutSection.vue";
-import CoachList from "../components/trainer/CoachList.vue";
-import AbonementsList from "@/components/abonements/AbonementsList.vue";
+import CoachList from "../components/sections/CoachList.vue";
+import AbonementsList from "@/components/sections/AbonementsList.vue";
 import HeaderSection from "@/components/sections/HeaderSection.vue";
 import SubscribeSection from "@/components/sections/SubscribeSection.vue";
 import PoolSection from "@/components/sections/PoolSection.vue";
 import WomenSection from "@/components/sections/WomenSection.vue";
+import FAQ from "@/components/sections/FAQ.vue";
 </script>
 
 <template>
@@ -15,6 +16,7 @@ import WomenSection from "@/components/sections/WomenSection.vue";
   <AbonementsList />
   <WomenSection />
   <CoachList />
+  <FAQ />
   <PoolSection />
   <SubscribeSection />
 </template>
