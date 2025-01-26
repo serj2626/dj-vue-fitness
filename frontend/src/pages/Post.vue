9637d73d87bb9aa2274d69c9e7acb5f235864253
serj2626 <serj2626@mail.ru>
2025-01-26 23:11:36 +0300

    style: del other code and classes

--- frontend/src/pages/Post.vue
+++ frontend/src/pages/Post.vue
@@ -1,45 +1,43 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import axios from 'axios';
-import { ref, onMounted } from 'vue';
-import { useToast } from 'vue-toastification';
-import { useRoute } from 'vue-router';
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import { useToast } from "vue-toastification";
+import { useRoute } from "vue-router";
 
 interface IPost {
-    id: number;
-    title: string;
-    content: string;
-    created_at : string;
-    updated_at : string;
-    category: string;
+  id: number;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+  category: string;
 }
 
 const route = useRoute();
-const { category } = route.params
+const { category } = route.params;
 const toast = useToast();
 const post = ref<IPost>({} as IPost);
 
 const getPost = async () => {
-    try {
-        const res = await axios.get(`/api/workout/posts/${category}`);
-        console.log(res.data);
-        post.value = res.data;
-    } catch {
-        toast.error('Произошла ошибка при получении постов');
-    }
-}
+  try {
+    const res = await axios.get(`/api/workout/posts/${category}`);
+    console.log(res.data);
+    post.value = res.data;
+  } catch {
+    toast.error("Произошла ошибка при получении постов");
+  }
+};
 
 onMounted(getPost);
 </script>
 
 <template>
-      <ArrowUpButton />
-    <div class="container row-2xl rounded-xl bg-white p-5 mb-12">
-        <h1 class="text-center mt-5 mb-20 uppercase font-bold">{{ post.title }}</h1>
-        <p v-html="post.content"></p>
-
-    </div>
-
+  <ArrowUpButton />
+  <div class="container rounded-xl bg-white p-5 mb-24 mt-[100px]">
+    <h1 class="text-center mt-5 mb-20 uppercase font-bold">{{ post.title }}</h1>
+    <p v-html="post.content"></p>
+  </div>
 </template>
 
 <style scoped></style>
