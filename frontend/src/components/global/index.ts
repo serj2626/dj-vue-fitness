a3d279e717ddea9527ccef024da87e39a508542b
serj2626 <serj2626@mail.ru>
2025-01-27 15:32:31 +0300

    style: del other code

--- frontend/src/components/global/index.ts
+++ frontend/src/components/global/index.ts
@@ -1,19 +1,17 @@
+import Alert from "./Alert.vue";
 import type { App } from "vue";
 import ArrowUpButton from "./ArrowUpButton.vue";
-import URating from "./URating.vue";
-import UButton from "./UButton.vue";
 import AuthCard from "./AuthCard.vue";
-import CreateOrderModal from "./CreateOrderModal.vue";
-import UModal from "./UModal.vue";
 import DeleteModal from "./DeleteModal.vue";
-import Alert from "./Alert.vue";
+import UButton from "./UButton.vue";
+import UModal from "./UModal.vue";
+import URating from "./URating.vue";
 
 const components = [
   { name: "ArrowUpButton", component: ArrowUpButton },
   { name: "URating", component: URating },
   { name: "UButton", component: UButton },
   { name: "AuthCard", component: AuthCard },
-  { name: "CreateOrderModal", component: CreateOrderModal },
   { name: "UModal", component: UModal },
   { name: "DeleteModal", component: DeleteModal },
   { name: "Alert", component: Alert },
