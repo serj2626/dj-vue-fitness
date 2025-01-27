91df2118ae2fe8cbe03b3c83d394f9b002659ff1
serj2626 <serj2626@mail.ru>
2025-01-27 13:27:20 +0300

    feat: new func for get path by all images trainer

--- backend/workout/service.py
+++ backend/workout/service.py
@@ -1,2 +1,6 @@
 def get_path_for_avatar_for_trainer(instance, filename):
     return f"trainers/{instance.position}/{instance.first_name}{instance.last_name}/{filename}"
+
+
+def get_path_for_all_images_by_trainer(instance, filename):
+    return f"trainers/{instance.trainer.position}/{instance.trainer.first_name}{instance.trainer.last_name}/gallery/{filename}"
