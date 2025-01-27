f0ae784efdbab5e55c2eeba66b18d7350eae95e1
serj2626 <serj2626@mail.ru>
2025-01-27 15:34:35 +0300

    feat: add galleria

--- frontend/src/pages/CoachDetail.vue
+++ frontend/src/pages/CoachDetail.vue
@@ -3,13 +3,18 @@ import axios from "axios";
 import { ref, onMounted, reactive } from "vue";
 import { useToast } from "vue-toastification";
 import { useRoute, useRouter } from "vue-router";
-import { FwbRating } from "flowbite-vue";
-import { FwbButton } from "flowbite-vue";
+import { FwbRating, FwbButton } from "flowbite-vue";
 import type { iRate, ITrainer } from "@/types/workout";
 import { checkDateOrderTraining } from "@/utils/validators";
 import { MESSAGES } from "@/types/messages";
 import Icon from "@/components/Icon.vue";
-import TrainerImages from "@/components/trainer/TrainerImages.vue";
+import { AppRoutes } from "@/utils/router";
+import Galleria from "primevue/galleria";
+import { useCoachesStore } from "@/stores/coaches";
+import { storeToRefs } from "pinia";
+
+const store = useCoachesStore();
+const { responsiveOptions } = storeToRefs(store);
 
 const router = useRouter();
 const route = useRoute();
@@ -66,6 +71,8 @@ const getRates = async () => {
   }
 };
 
+const displayBasic = ref(false);
+
 onMounted(() => {
   getCoach();
   getRates();
@@ -73,60 +80,72 @@ onMounted(() => {
 </script>
 
 <template>
-
-
-
-  <div class="container my-[160px] rounded-xl shadow-2xl shadow-white">
-    <div class="flex max-[767px]:flex-col gap-4 p-9">
+  <!-- asdasdas -->
+  <div class="container mt-[100px] rounded-xl shadow-2xl shadow-white">
+    <div class="grid grid-cols-2 max-[640px]:grid-cols-1  gap-4 p-5">
       <div
-        class="min-[767px]:w-1/3 mx-auto rounded-2xl flex flex-col items-center justify-center gap-8"
+        class="rounded-2xl py-5 px-12 flex flex-col items-center justify-center"
       >
         <img class="rounded-xl" :src="coach.avatar" alt="" />
-        <fwb-rating :rating="coach.total_rating" />
+        <div class="flex flex-col items-center my-10">
+          <fwb-rating :rating="coach.total_rating" />
 
-        <div>
-          <a class="text-white cursor-pointer">
-            Отзывы
-            <span
-              @click="
-                router.push({ name: 'coachReviews', params: { id: coach.id } })
-              "
-              class="text-yellow-300 hover:underline"
-            >
-              ( {{ coach.count_reviews }} )</span
-            >
-          </a>
+          <div>
+            <a class="text-white cursor-pointer mt-3">
+              Отзывы
+              <span
+                @click="
+                  router.push({
+                    name: AppRoutes.coachReviews,
+                    params: { id: coach.id },
+                  })
+                "
+                class="text-yellow-300 hover:underline"
+              >
+                ( {{ coach.count_reviews }} )</span
+              >
+            </a>
+          </div>
         </div>
-        <fwb-button
-          @click="showModal = true"
-          color="yellow"
-          class="block w-full text-lg p-3"
-          >Записаться
-        </fwb-button>
+        <div class="flex flex-col gap-3 w-full">
+          <fwb-button
+            @click="showModal = true"
+            color="yellow"
+            class="block w-full text-lg p-3"
+            >Записаться
+          </fwb-button>
+          <fwb-button
+            v-if="coach?.images?.length"
+            @click="displayBasic = true"
+            color="purple"
+            class="block w-full text-lg p-3"
+            >Фотографии
+          </fwb-button>
+        </div>
       </div>
 
-      <div class="min-[767px]:w-2/3 text-white">
-        <ul class="coach-info">
+      <div class="text-white py-5 px-12">
+        <ul class="coach-info ">
           <li>
-            <div class="flex gap-5">
+            <div class="flex items-center gap-5">
               <Icon type="user" />
               {{ coach.first_name }} {{ coach.last_name }}
             </div>
           </li>
           <li>
-            <div class="flex gap-5">
+            <div class="flex items-center gap-5">
               <Icon type="position" />
               {{ coach.position }}
             </div>
           </li>
           <li>
-            <div class="flex gap-5">
+            <div class="flex items-center gap-5">
               <Icon type="email" />
               {{ coach.email }}
             </div>
           </li>
           <li>
-            <div class="flex gap-5">
+            <div class="flex items-center gap-5">
               <Icon type="phone" />
               {{ coach.phone }}
             </div>
@@ -139,7 +158,9 @@ onMounted(() => {
         </ul>
       </div>
     </div>
-    <div class="grid grid-cols-3 gap-3 max-[500px]:grid-cols-1 max-[767px]:grid-cols-2">
+    <!-- <div
+      class="grid grid-cols-3 gap-3 max-[500px]:grid-cols-1 max-[767px]:grid-cols-2"
+    >
       <img
         v-for="img in coach.images"
         :key="img.id"
@@ -148,13 +169,36 @@ onMounted(() => {
         alt="photo"
         loading="lazy"
       />
-    </div>
-    <!-- <div>
-      <TrainerImages :images="coach.images" />
     </div> -->
+
+    <Galleria
+      v-model:visible="displayBasic"
+      :value="coach.images"
+      :responsiveOptions="responsiveOptions"
+      :numVisible="9"
+      containerStyle="max-width: 50%; w-full"
+      :circular="true"
+      :fullScreen="true"
+      :showItemNavigators="true"
+    >
+      <template #item="slotProps">
+        <img
+          :src="slotProps.item.image"
+          :alt="slotProps.item.alt"
+          style="width: 100%; height: 70vh; display: block"
+        />
+      </template>
+      <template #thumbnail="slotProps">
+        <img
+          :src="slotProps.item.image"
+          :alt="slotProps.item.alt"
+          style="display: block; height: 200px; width: 100%"
+        />
+      </template>
+    </Galleria>
   </div>
 
-  <CreateOrderModal
+  <UModal
     v-if="showModal"
     v-model:date="trainingData.date"
     v-model:rate="trainingData.rate"
@@ -179,7 +223,7 @@ onMounted(() => {
 }
 
 .coach-info li {
-  width: 50%;
+  width: 100%;
   background-color: rgb(35, 31, 31);
   border: none;
   box-shadow: 0 0 15px white;
@@ -191,7 +235,7 @@ onMounted(() => {
   transition: all 0.3s ease-in;
 
   &:hover {
-    box-shadow: 0 0 25px white;
+    box-shadow: 0 0 35px 3px white;
   }
 }
 
@@ -199,5 +243,4 @@ ul {
   margin: 0;
   padding: 0;
 }
-
 </style>
