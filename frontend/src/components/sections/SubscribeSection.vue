014e6e9e06e48f223658edd5cf8d9373956b561c
serj2626 <serj2626@mail.ru>
2025-01-27 13:31:14 +0300

    feat: update color by title

--- frontend/src/components/sections/SubscribeSection.vue
+++ frontend/src/components/sections/SubscribeSection.vue
@@ -26,7 +26,7 @@ const sendSubscription = async () => {
   <section id="contacts" class="subscribe px-6 pb-3">
     <div class="relative overflow-hidden px-6 py-24">
       <h2
-        class="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl"
+        class="mx-auto max-w-2xl text-center text-3xl font-bold text-yellow-300 sm:text-4xl"
       >
         Будьте в курсе событий
       </h2>
@@ -48,7 +48,7 @@ const sendSubscription = async () => {
           required
           placeholder="Введите почту"
         />
-        <fwb-button  size="sm" color="light">Подписаться</fwb-button>
+        <fwb-button size="sm" color="light">Подписаться</fwb-button>
       </form>
 
       <Footer />
