c1adb97cffd4b60616d6503fa0a706bb7215e0e9
serj2626 <serj2626@mail.ru>
2025-01-27 15:34:15 +0300

    feat: add import store

--- frontend/src/pages/AbonementsLIst.vue
+++ frontend/src/pages/AbonementsLIst.vue
@@ -5,11 +5,16 @@ import { useRouter } from "vue-router";
 import { useToast } from "vue-toastification";
 import type { IAbonement } from "@/types/orders";
 import { checkDateOrderAbonement } from "@/utils/validators";
+import { useAbonementsStore } from "@/stores/abonements";
+import { storeToRefs } from "pinia";
+
+const store = useAbonementsStore();
+const { abonements } = storeToRefs(store);
+const { getAbonements } = store;
 
 const router = useRouter();
 const toast = useToast();
-const abonements = ref<IAbonement[]>([]);
-const orderDate = ref<string>('');
+const orderDate = ref<string>("");
 
 const showModal = ref<boolean>(false);
 
@@ -42,29 +47,26 @@ const newOrderAbonement = async () => {
   }
 };
 
-const getAbonements = async () => {
-  try {
-    const { data } = await axios.get("/api/orders/abonements/");
-    abonements.value = data;
-  } catch {
-    toast.error("Произошла ошибка");
-  }
-};
-
 onMounted(() => getAbonements());
 </script>
 
 <template>
   <div class="container row mt-52">
     <div class="abonements relative overflow-x-auto sm:rounded-lg">
-      <table class="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400">
+      <table
+        class="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400"
+      >
         <thead
-          class="text-xs text-white border-2 border-opacity-25 border-white uppercase bg-gray-300 bg-transparent dark:bg-gray-700 dark:text-gray-400">
+          class="text-xs text-white border-2 border-opacity-25 border-white uppercase bg-gray-300 bg-transparent dark:bg-gray-700 dark:text-gray-400"
+        >
           <tr>
             <th scope="col" class="p-4">
               <div class="hidden">
-                <input id="checkbox-all" type="checkbox"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                <input
+                  id="checkbox-all"
+                  type="checkbox"
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
                 <label for="checkbox-all" class="sr-only">checkbox</label>
               </div>
             </th>
@@ -76,17 +78,27 @@ onMounted(() => getAbonements());
           </tr>
         </thead>
         <tbody>
-          <tr v-for="abonement in abonements" :key="abonement.id"
-            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+          <tr
+            v-for="abonement in abonements"
+            :key="abonement.id"
+            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+          >
             <td class="w-4 p-4">
               <div class="flex items-center">
-                <input @click="choiceAbonement(abonement)" id="checkbox-table-1" type="checkbox"
+                <input
+                  @click="choiceAbonement(abonement)"
+                  id="checkbox-table-1"
+                  type="checkbox"
                   :checked="selectAbonement.id === abonement.id"
-                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                  class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
                 <label for="checkbox-table-1" class="sr-only">checkbox</label>
               </div>
             </td>
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
               {{ abonement.title }}
             </th>
             <td class="px-6 py-4">
@@ -100,15 +112,22 @@ onMounted(() => getAbonements());
             </td>
             <td class="px-6 py-4">
               <!-- Modal toggle -->
-              <button v-if="selectAbonement.id === abonement.id" @click="showModal = true" title="Забронировать"
+              <button
+                v-if="selectAbonement.id === abonement.id"
+                @click="showModal = true"
+                title="Забронировать"
                 class="block text-white bg-transparent font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                type="button">
+                type="button"
+              >
                 <i class="fa-solid fa-plus fa-2xl" style="color: #ff5900"></i>
               </button>
 
-              <button v-else disabled
+              <button
+                v-else
+                disabled
                 class="block text-white bg-transparent font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                type="button">
+                type="button"
+              >
                 <i class="fa-solid fa-minus fa-2xl" style="color: #0011ff"></i>
               </button>
             </td>
@@ -118,14 +137,14 @@ onMounted(() => getAbonements());
     </div>
   </div>
 
-  <CreateOrderModal 
-      v-if="showModal" 
-      :abonement="selectAbonement" 
-      @closeModal="showModal = false" 
-      orderType="abonement"
-      v-model:date="orderDate" 
-      @crateOrderAbonement="newOrderAbonement" 
-    />
+  <UModal
+    v-if="showModal"
+    :abonement="selectAbonement"
+    @closeModal="showModal = false"
+    orderType="abonement"
+    v-model:date="orderDate"
+    @crateOrderAbonement="newOrderAbonement"
+  />
 </template>
 <style scoped>
 .abonements {
