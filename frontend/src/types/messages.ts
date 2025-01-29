3747716af7c7cbd77057563953bb67cefad63091
serj2626 <serj2626@mail.ru>
2025-01-30 01:52:51 +0300

    feat: new messages

--- frontend/src/types/messages.ts
+++ frontend/src/types/messages.ts
@@ -1,9 +1,14 @@
 export const enum MESSAGES {
   ABONEMENT_CREATED = "Вы забронировали абонемент",
   ABONEMENT_DELETED = "Абонемент удален",
+  ABONEMENT_DELETED_ERROR = "Произошла ошибка при удалении абонемента",
+  NO_ABONEMENTS = "На данный момент у вас нет абонементов",
+
   AUTH_ERROR = "Вы не авторизовались",
+
   COACH_ERROR = "Произошла ошибка при получении тренера",
   COACHES_ERROR = "Произошла ошибка при получении тренеров",
+
   DATA_ERROR = "Произошла ошибка при получении данных",
   DATE_ERROR = "Дата бронирования не может быть меньше текущей",
   DATE_START_SELECT = "Выберите дату начала абонемента",
@@ -11,17 +16,22 @@ export const enum MESSAGES {
   FILL_FIELD = "Заполните поле",
   LOGIN_SUCCESS = "Вы успешно вошли в аккаунт",
   LOGOUT_SUCCESS = "Вы вышли из аккаунта",
-  NO_ABONEMENTS = "На данный момент у вас нет абонементов",
+
   NO_COACHES = "Тренеры не найдены",
   NO_REVIEWS = "Комментариев нет",
-  NO_TRAININGS = "На данный момент у вас нет тренировок",
+
   POSTS_ERROR = "Произошла ошибка при получении постов",
   RATE_SELECT = "Выберите тариф",
   REGISTER_SUCCESS = "Вы успешно зарегистрировались",
+
   REVIEW_CREATED = "Отзыв успешно создан",
   REVIEW_CREATED_ERROR = "Произошла ошибка при создании отзыва",
   REVIEW_DELETED = "Отзыв успешно удален",
   REVIEW_DELETED_ERROR = "Произошла ошибка при создании отзыва",
+  REVIEW_ERROR_AUTH = "Оставлять отзыв могут только авторизованные пользователи",
+
   TRAINING_CREATED = "Вы записались на тренировку",
   TRAINING_DELETED = "Тренировка удалена",
+  TRAINING_DELETED_ERROR = "Произошла ошибка при удалении тренировки",
+  NO_TRAININGS = "На данный момент у вас нет тренировок",
 }
