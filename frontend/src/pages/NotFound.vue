aff896782e1ea2cc1fce174f53ff00dd2e793cee
serj2626 <serj2626@mail.ru>
2025-01-30 01:53:12 +0300

    refactor: add moke data

--- frontend/src/pages/NotFound.vue
+++ frontend/src/pages/NotFound.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
@@ -34,4 +34,74 @@ const router = useRouter();
 h1{
     text-shadow: 0 0 15px rgb(219, 217, 217);
 }
-</style>
+</style> -->
+
+<template>
+  <div class="card flex justify-center">
+    <Galleria
+      v-model:visible="displayBasic"
+      :value="images"
+      :responsiveOptions="responsiveOptions"
+      :numVisible="9"
+      containerStyle="max-width: 50%"
+      :circular="true"
+      :fullScreen="true"
+      :showItemNavigators="true"
+    >
+      <template #item="slotProps">
+        <img
+          :src="slotProps.item"
+          :alt="slotProps.item.alt"
+          style="width: 100%; display: block"
+        />
+      </template>
+      <template #thumbnail="slotProps">
+        <img
+          :src="slotProps.item"
+          :alt="slotProps.item.alt"
+          style="display: block"
+        />
+      </template>
+    </Galleria>
+
+    <Button
+      label="Show"
+      class="mt-56"
+      icon="pi pi-external-link"
+      @click="displayBasic = true"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import one from "../assets/img/coach/girl.png"
+import two from "../assets/img/coach/girl2.png"
+import three from "../assets/img/coach/man.png"
+import foue from "../assets/img/coach/man2.png"
+import Galleria from "primevue/galleria";
+import Button from "primevue/button";
+
+const images = ref([one, two, three, foue]);
+const responsiveOptions = ref([
+  {
+    breakpoint: "1500px",
+    numVisible: 5,
+  },
+  {
+    breakpoint: "1024px",
+    numVisible: 3,
+  },
+  {
+    breakpoint: "768px",
+    numVisible: 2,
+  },
+  {
+    breakpoint: "560px",
+    numVisible: 1,
+  },
+]);
+const displayBasic = ref(false);
+
+
+</script>
